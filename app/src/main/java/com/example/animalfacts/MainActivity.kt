3af064890758a5044d68e159614ac187d25241dd
dd <duddaniel0919@gmail.com>
2023-06-22 15:02:58 +0200

    add recyclerview field

--- app/src/main/java/com/example/animalfacts/MainActivity.kt
+++ app/src/main/java/com/example/animalfacts/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.animalfacts
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
