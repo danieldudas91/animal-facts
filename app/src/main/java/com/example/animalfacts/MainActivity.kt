be6da27b144616ce06e0145244ff76f72b5f84cc
dd <duddaniel0919@gmail.com>
2023-06-22 15:09:20 +0200

    initialize recyclerview inside onCreate method

--- app/src/main/java/com/example/animalfacts/MainActivity.kt
+++ app/src/main/java/com/example/animalfacts/MainActivity.kt
@@ -13,6 +13,9 @@ class MainActivity() : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.recyclerview)
+        recyclerView.adapter = AnimalViewAdapter(createAnimalArray())
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
     private fun createAnimalArray(): Array<Animal> {
         return arrayOf(Animal("Lion"), Animal("Tiger"), Animal("Zebra"))
