6dceb6528cfe9231eefa739976c819b1183a8962
dd <duddaniel0919@gmail.com>
2023-06-24 19:34:38 +0200

    create and implement getData function for fetching data from API

--- app/src/main/java/com/example/animalfacts/AnimalViewModel.kt
+++ app/src/main/java/com/example/animalfacts/AnimalViewModel.kt
@@ -2,9 +2,29 @@ package com.example.animalfacts
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class AnimalViewModel: ViewModel(){
     val animals: MutableLiveData<List<AnimalResponse>> = MutableLiveData()
     var animalName: String = ""
+
+    private fun getData(){
+        if (animalName != ""){
+            val call: Call<List<AnimalResponse>> = ApiConfig.getApiService().getAnimalData(animalName)
+            call.enqueue(object : Callback<List<AnimalResponse>> {
+                override fun onResponse(
+                    call: Call<List<AnimalResponse>>,
+                    response: Response<List<AnimalResponse>>
+                ) {
+                    animals.postValue(response.body())
+                }
+                override fun onFailure(call: Call<List<AnimalResponse>>, t: Throwable) {
+                    t.printStackTrace()
+                }
+            })
+        }
+    }
 }
