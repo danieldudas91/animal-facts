68f10907cf1db164e8dbd7056e15aa36996e69a6
dd <duddaniel0919@gmail.com>
2023-06-24 19:41:36 +0200

    add EditText and button

--- app/src/main/java/com/example/animalfacts/MainActivity.kt
+++ app/src/main/java/com/example/animalfacts/MainActivity.kt
@@ -3,16 +3,23 @@ package com.example.animalfacts
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity() : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var button: Button
+    private val animalViewModel = AnimalViewModel()
+    private lateinit var  input: EditText
 
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        input = findViewById(R.id.input)
+        button = findViewById(R.id.button)
         recyclerView = findViewById(R.id.recyclerview)
         recyclerView.adapter = AnimalViewAdapter(createAnimalArray())
         recyclerView.layoutManager = LinearLayoutManager(this)
