14d61909a3031991d3cd233396a512494fa74793
dd <duddaniel0919@gmail.com>
2023-09-19 17:05:22 +0200

    create AnimalDetailsActivity

---
+++ app/src/main/java/com/example/animalfacts/AnimalDetailsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.animalfacts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class AnimalDetailsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
