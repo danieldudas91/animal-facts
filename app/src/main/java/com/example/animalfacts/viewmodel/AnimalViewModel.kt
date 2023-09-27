a8d0a0516782a19e735c208cb6855445a108fb92
dd <duddaniel0919@gmail.com>
2023-09-27 12:45:13 +0200

    add @HiltViewModel annotation and inject ApiService to constructor

--- app/src/main/java/com/example/animalfacts/viewmodel/AnimalViewModel.kt
+++ app/src/main/java/com/example/animalfacts/viewmodel/AnimalViewModel.kt
@@ -3,16 +3,20 @@ package com.example.animalfacts.viewmodel
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.example.animalfacts.AnimalResponseCache
-import com.example.animalfacts.ApiConfig
+import com.example.animalfacts.ApiService
 import com.example.animalfacts.model.AnimalResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.await
+import javax.inject.Inject
 
-
-class AnimalViewModel: ViewModel(){
+@HiltViewModel
+class AnimalViewModel @Inject constructor(
+    private val apiService: ApiService
+): ViewModel(){
     val animals = mutableStateListOf<AnimalResponse>()
     var animalName: String = ""
         set(value) {
@@ -31,7 +35,7 @@ class AnimalViewModel: ViewModel(){
                     animals.addAll(animalsInCache)
                     return@launch
                 }
-                val animalResponseData = ApiConfig.getApiService().getAnimalData(animalName).await()
+                val animalResponseData = apiService.getAnimalData(animalName).await()
                 animals.clear()
                 animals.addAll(animalResponseData)
                 cache[animalName] = animalResponseData
