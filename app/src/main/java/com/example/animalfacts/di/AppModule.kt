dac636aa927d462037fe982f79e5b08a8299503c
dd <duddaniel0919@gmail.com>
2023-09-27 12:42:59 +0200

    implement provider functions for ApiService

--- app/src/main/java/com/example/animalfacts/di/AppModule.kt
+++ app/src/main/java/com/example/animalfacts/di/AppModule.kt
@@ -3,9 +3,40 @@ package com.example.animalfacts.di
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import dagger.Provides
+import com.example.animalfacts.ApiService
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideBaseUrl(): String{
+        return "https://api.api-ninjas.com/v1/"
+    }
+    @Provides
+    @Singleton
+    fun provideApiService(baseUrl: String): ApiService{
+        val loggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
 
+        val client = OkHttpClient
+            .Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit
+            .Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
 }
