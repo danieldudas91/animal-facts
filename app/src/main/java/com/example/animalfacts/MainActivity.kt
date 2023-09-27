bf25b4e48f91a44e47f01f24d8988cff7c9b2471
dd <duddaniel0919@gmail.com>
2023-09-27 14:13:04 +0200

    create Composables of implemented UI elements

--- app/src/main/java/com/example/animalfacts/MainActivity.kt
+++ app/src/main/java/com/example/animalfacts/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.animalfacts
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,71 +28,94 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.animalfacts.model.AnimalResponse
 import com.example.animalfacts.viewmodel.AnimalViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val myContext = LocalContext.current
             val viewModel = hiltViewModel<AnimalViewModel>()
-            Column (
+            Column(
                 horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                var textFieldState by remember { mutableStateOf("") }
-                var lazyColumnState =  viewModel.animals
+            ) {
+                var lazyColumnState = viewModel.animals
+                var state = inputField("Enter animal name")
 
-
-                TextField(
-                    value = textFieldState,
-                    label = {
-                        Text("Enter animal name")
-                    },
-                    onValueChange = {
-                        textFieldState = it
-                    },
-                    singleLine = true,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(20.dp),
-                )
-                Button(
-                    onClick = {
-                        Log.d("Tag", textFieldState)
-                        viewModel.animalName = textFieldState
-                    },
-                ) {
-                    Text("Search")
-                }
-                LazyColumn (
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    itemsIndexed(
-                        lazyColumnState
-                    ) { _, animal ->
-
-                            ClickableText(
-                                onClick = {
-                                    val intent = Intent(myContext, AnimalDetailsActivity::class.java)
-                                    intent.putExtra("animal", animal)
-                                    myContext.startActivity(intent)
-                                },
-                                text = AnnotatedString(animal.name.toString()),
-                                modifier = Modifier.fillMaxWidth(),
-                                style = TextStyle(
-                                    color = Color.Black,
-                                    fontSize = 16.sp,
-                                    textAlign = TextAlign.Center
-                                )
-                            )
-                    }
-                }
+                MyButton(text = "Search", onClick = {
+                    viewModel.animalName = state
+                })
+                ClickableList(stateList = lazyColumnState, context = myContext)
             }
 
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun inputField(text: String): String {
+    var textFieldState by remember { mutableStateOf("") }
+    TextField(
+        value = textFieldState,
+        label = {
+            Text(text)
+        },
+        onValueChange = {
+            textFieldState = it
+        },
+        singleLine = true,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+    )
+    return textFieldState
+}
+
+@Composable
+fun ClickableList(
+    stateList: SnapshotStateList<AnimalResponse>,
+    context: Context
+) {
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        itemsIndexed(
+            stateList
+        ) { _, animal ->
+
+            ClickableText(
+                onClick = {
+                    Intent(context, AnimalDetailsActivity::class.java).apply {
+                        putExtra("animal", animal)
+                    }.also {
+                        context.startActivity(it)
+                    }
+                },
+                text = AnnotatedString(animal.name.toString()),
+                modifier = Modifier.fillMaxWidth(),
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun MyButton(
+    text: String,
+    onClick: () -> Unit
+){
+    Button(
+        onClick = onClick,
+    ) {
+        Text(text)
     }
 }
