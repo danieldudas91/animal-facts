01a8dd7fb242cac0c3ca3c1c3f2b2608437042df
dd <duddaniel0919@gmail.com>
2023-09-28 11:07:57 +0200

    create Composables for UI elements

--- app/src/main/java/com/example/animalfacts/AnimalDetailsActivity.kt
+++ app/src/main/java/com/example/animalfacts/AnimalDetailsActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.Composable
 import com.example.animalfacts.model.AnimalResponse
+import com.example.animalfacts.model.Characteristics
+import com.example.animalfacts.model.Taxonomy
+import kotlin.reflect.full.memberProperties
 
 @Suppress("DEPRECATION")
 class AnimalDetailsActivity : ComponentActivity() {
@@ -18,20 +20,48 @@ class AnimalDetailsActivity : ComponentActivity() {
             val intent = intent
             val animal = intent.getSerializableExtra("animal") as AnimalResponse
 
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = animal.name.toString(),
-                    )
-                Text(text = animal.taxonomy?.scientificName.toString())
-                Text(text = "Taxonomy")
-                Text(text = animal.taxonomy.toString())
-                Text(text = "Characteristics")
-                Text(
-                    textAlign = TextAlign.Center,
-                    text = animal.characteristics.toString())
+            Column {
+                TaxonomyText(taxonomy = animal.taxonomy)
+                CharacteristicsText(characteristics = animal.characteristics)
+                LocationsText(locations = animal.locations)
             }
         }
+    }
+}
+
+@Composable
+fun TaxonomyText(taxonomy: Taxonomy?) {
+    val taxonomyProperties = Taxonomy::class.memberProperties
+    Column {
+        taxonomyProperties.forEach {
+            Row {
+                Text(text = "${it.name} :")
+                if (taxonomy != null) {
+                    Text(text = it.get(taxonomy).toString())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacteristicsText(characteristics: Characteristics?) {
+    val characteristicsProperties = Characteristics::class.memberProperties
+    Column {
+        characteristicsProperties.forEach {
+            Row {
+                Text(text = "${it.name} :")
+                if (characteristics != null) {
+                    Text(text = it.get(characteristics).toString())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LocationsText(locations: List<String?>?) {
+    if(locations != null){
+        Text(text = locations.joinToString(", "))
     }
 }
